f1684f0e515210d315bc17abcbd9c21618a3a734
sahilaskar2 <sahilaskar2@gmail.com>
2026-03-12 22:40:50 +0530

    Added username investigation feature

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+sites = {
+    "GitHub": "https://github.com/{}",
+    "Reddit": "https://www.reddit.com/user/{}",
+    "Instagram": "https://www.instagram.com/{}",
+    "Twitter": "https://twitter.com/{}"
+}
+
+@app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("index.html")
+
+    results = {}
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+
+        for site, url in sites.items():
+
+            profile_url = url.format(username)
+
+            r = requests.get(profile_url)
+
+            if r.status_code == 200:
+                results[site] = "✓ Found"
+            else:
+                results[site] = "✗ Not Found"
+
+    return render_template("index.html", results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
